be1a52c8fa0f2bfe3e157376c96533f4060644d2
hamzasherxdxd <hamzasher223@gmail.com>
2022-02-17 13:43:06 +0500

    DB FIX

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -137,3 +137,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
