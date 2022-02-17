bfd25c6ff96f9ed66552a1dd3749c2d8bf982e7e
hamzasherxdxd <hamzasher223@gmail.com>
2022-02-17 11:40:43 +0500

    Changed Database

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -80,13 +80,18 @@ WSGI_APPLICATION = 'music_controller.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'musicshare',
-        'USER': 'hamzasherxdxd',
-        'PASSWORD': 'hamzasherkhan123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'musicshare',
+    #     'USER': 'hamzasherxdxd',
+    #     'PASSWORD': 'hamzasherkhan123',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
+        
 }
 
 
