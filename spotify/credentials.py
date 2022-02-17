e0d2c619a2a37342db0d43474778e349ca9fb253
hamzasherxdxd <hamzasher223@gmail.com>
2022-02-17 16:52:14 +0500

    Fixed redirect problem

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "054fea955667454ca0e9fe5566685fa0"
 CLIENT_SECRET = "e78096f31f9740e899a931e132073d63"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "https://hamzasmusicshareapp.herokuapp.com/spotify/redirect"
