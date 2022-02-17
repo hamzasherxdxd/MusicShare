12af8d00ca794ff6f07e6ef319f9c53852f4d9ed
hamzasherxdxd <hamzasher223@gmail.com>
2022-02-17 13:50:53 +0500

    Fixed DB

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'music_controller.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'musicshare',
-    #     'USER': 'hamzasherxdxd',
-    #     'PASSWORD': 'hamzasherkhan123',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'musicshare',
+        'USER': 'hamzasherxdxd',
+        'PASSWORD': 'hamzasherkhan123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
         
 }
 
@@ -137,7 +137,3 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
-
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
